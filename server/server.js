bbcd9c24f7ca0dd15bc35ef361849001d8d7f2b0
HxArmus <kaustubhjawde@gmail.com>
2026-02-09 22:48:29 +0530

    added some fixes before deployment

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 import cors from "cors"
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index"
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 app.use(express.json());
@@ -16,4 +16,4 @@ app.get("/",(req,res)=>res.send("server is Live !"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT =process.env.PORT || 3000;
-app.listen(PORT,()=>{`server running on port ${PORT}`})
+app.listen(PORT,()=>{console.log(`server running on port ${PORT}`)})
