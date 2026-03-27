88943efea7633416b84722d3f888c8800ac34d7e
HxArmus <kaustubhjawde@gmail.com>
2026-03-27 14:45:00 +0530

    fixed home Folder name in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes,Route, useLocation } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home';
 import Marketplace from './pages/Marketplace'
 import MyListing from './pages/MyListing'
 import Listingdetails from './pages/ListingDetails'
